caa1399f239ebbc703151825c33fbc476742a910
Wolfie <kevyan76@gmail.com>
2020-02-15 21:23:16 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
-console.log("Up and running!");
+/*console.log("Up and running!");
 let cardOne = "queen";
 let cardTwo = "queen";
 let cardThree = "king";
 let cardFour = "king";
 console.log("User flipped " + cardOne);
 console.log("User flipped " + cardThree);
+*/
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2);{
+
+	if (cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match");	
+	}
+
+	else if (cardsInPlay[0] !== cardsInPlay[1]){
+		alert("Sorry, try again");
+	}
+}
+
