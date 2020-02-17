866cfc105792ee90ae0127e9e46aa7f2e2d3c41f
Wolfie <kevyan76@gmail.com>
2020-02-17 14:52:27 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,29 +1,23 @@
-/*console.log("Up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
-*/
-const cards = ["queen", "queen", "king", "king"];
-const cardsInPlay = [];
-const cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
 
-const cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+	console.log("You found a match!");
+} else {
+  	console.log("Sorry, try again.");
+}
+}
 
-if (cardsInPlay.length === 2);{
+function flipcard(cardId) {
+  cardsInPlay.push(cardId);
+  console.log("User flipped " + cards[cardId]);
 
-	if (cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match");	
-	}
-
-	else if (cardsInPlay[0] !== cardsInPlay[1]){
-		alert("Sorry, try again");
+  if (cardsInPlay.length === 2) {
+	checkForMatch();
 	}
 }
+
+flipcard(0);
+flipcard(2);
 
